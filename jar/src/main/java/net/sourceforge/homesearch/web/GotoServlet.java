612411386851852a0b81efeca08396f854f64599
vitaly.sazanovich <Vitaly.Sazanovich@gmail.com>
2013-10-29 14:52:24 +0000

    Preparing for release.

--- jar/src/main/java/net/sourceforge/homesearch/web/GotoServlet.java
+++ jar/src/main/java/net/sourceforge/homesearch/web/GotoServlet.java
@@ -12,7 +12,9 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Author: Vitaly Sazanovich
@@ -21,31 +23,46 @@ import java.util.List;
  * Time: 19:52
  */
 public class GotoServlet extends HttpServlet {
+    static Map<String, List<IndexSet>> INDEX_SETS = new HashMap<>();
 
 
     protected void doGet(HttpServletRequest request,
                          HttpServletResponse response) throws ServletException, IOException {
         try {
             String q = request.getParameter("q");
+            String lemId = "";
+            String bId = request.getParameter("betid");
+            if (q.startsWith("_")) {
+                lemId = q.split("_")[1].split("gwna")[1];
+            }
             String from = request.getHeader("referer");
             String dicName = from.split(".zip")[0];
             dicName = dicName.substring(dicName.lastIndexOf("/") + 1);
-            String pathToIndex = "content/" + dicName + ".zip/" + dicName + ".ser";
-            File entry = new TFile(pathToIndex);
-            InputStream os = new TFileInputStream(entry);
-            ObjectInputStream ois = new ObjectInputStream(os);
-            List<IndexSet> l = (List<IndexSet>) ois.readObject();
-            ois.close();
-            os.close();
+
+            List<IndexSet> l = INDEX_SETS.get(dicName);
+            if (l == null) {
+                String pathToIndex = "content/" + dicName + ".zip/" + dicName + ".ser";
+                File entry = new TFile(pathToIndex);
+                InputStream os = new TFileInputStream(entry);
+                ObjectInputStream ois = new ObjectInputStream(os);
+                l = (List<IndexSet>) ois.readObject();
+                INDEX_SETS.put(dicName, l);
+                ois.close();
+                os.close();
+            }
+
 
             for (IndexSet is : l) {
                 String hw = is.headword;
                 if (hw.indexOf("<") != -1) {
                     hw = hw.split("<")[0];
                 }
-                if (hw.equals(q)) {
+                if (hw.equals(q) || (is.lemId!=null && is.lemId.equals(lemId))) {
                     int folder = is.realIndex / 1000 + 1;
                     String red = from.split(".zip")[0] + ".zip/" + folder + "/" + is.realIndex + ".xhtml";
+                    if (bId != null && !bId.equals("")) {
+                        red += "#" + bId;
+                    }
                     response.sendRedirect(red);
                     return;
                 }
